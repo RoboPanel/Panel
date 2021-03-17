04099f9a338f3d10821507236db690f7a45fec0c
Jamie Grimwood <volcanomonster07@gmail.com>
2021-03-17 11:42:43 -0700

    Initial Commit

---
+++ resources/views/adminhome.blade.php
@@ -0,0 +1,22 @@
+<?
+$latest = file_get_contents('https://robopanel.io/latestpanel.txt');
+?>
+
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You are running RoboPanel verison {{ config('app.version', 'Unable to find version!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
