c2cd20457e5f72a1d4e00a20fe32a04c687f7e15
Jamie Grimwood <volcanomonster07@gmail.com>
2021-03-19 12:09:19 -0700

    Updating Logo AGAIN

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="https://robopanel.io/logo.png" class="w-20 h-20 fill-current text-gray-500" />
                     </a>
                 </div>
 
